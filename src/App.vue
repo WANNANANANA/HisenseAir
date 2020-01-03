0fd57c06da1e1ef1b17ec034913b13d5da92520a
WANNA <2457684290@qq.com>
2020-01-03 13:42:20 +0800

    '主页面'

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,112 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <introduction></introduction>
+    <div class="icon">
+      <div class="left"></div>
+      <div class="right"></div>
     </div>
-    <router-view/>
+    <main>
+      <div class="plant">
+        <img src="./assets/img/seed.png" alt />
+      </div>
+      <div class="pot">
+        <img src="./assets/img/pot.png" alt />
+      </div>
+    </main>
+    <footer>
+      <div class="content">
+        <div class="footer-text">
+          <span>成长值{{growth}}</span>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
+<script>
+import introduction from "./components/introduction";
+export default {
+  components: { introduction },
+  data() {
+    return {
+      growth: 20
+    };
+  }
+};
+</script>
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 36px 8% 0 8%;
+  background-image: url("./assets/img/seed_bg.png");
+  background-size: 100% 100%;
+  .icon {
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    top: 46%;
+    left: 0px;
+    width: 100%;
+    padding: 0 8%;
+    height: 46px;
+    div {
+      width: 46px;
+      height: 46px;
+      background-size: 100% 100%;
+      &.left {
+        background-image: url("./assets/img/icon_left.png");
+      }
+      &.right {
+        background-image: url("./assets/img/icon_right.png");
+      }
+    }
+  }
+  main {
+    position: relative;
+    width: 100%;
+    height: 40%;
+    text-align: center;
+    .plant {
+      position: absolute;
+      left: 50%;
+      bottom: 0px;
+      transform: translate(-50%, 0);
+      img {
+        width: 120px;
+      }
+    }
+    .pot {
+      position: absolute;
+      right: 20px;
+      bottom: 20px;
+      img {
+        width: 76px;
+      }
+    }
+  }
+  footer {
+    position: relative;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    width: 100%;
+    height: 18%;
+    .content {
+      position: relative;
+      width: 84%;
+      height: 50px;
+      margin: 10% auto 0;
+      background-color: #cb8f51;
+      border-radius: 25px;
+      .footer-text {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        width: 80px;
+        height: 30px;
+        background: #fff;
+        transform: translate(0, -100%);
+      }
     }
   }
 }
---
+++ src/components/introduction.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="introduction">
+    <h3 class="title">智慧宏图展区介绍</h3>
+    <div class="content">
+      <p>这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话这是一段无限复制的话。</p>
+      <img src="../assets/img/seed_bg.png" alt />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "introduction",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less">
+.introduction {
+  position: relative;
+  width: 100%;
+  height: 42%;
+  border: 4px solid rgb(255, 166, 0);
+  border-radius: 12px;
+  padding-top: 30px;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.5);
+  h3 {
+    position: absolute;
+    top: 0px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgb(255, 166, 0);
+    width: 76%;
+    height: 32px;
+    border-radius: 16px 16px;
+    line-height: 32px;
+    text-align: center;
+  }
+  .content {
+    width: 98%;
+    height: 100%;
+    font-size: 15px;
+    padding: 0px 20px 0px 20px;
+    overflow: scroll;
+    &::-webkit-scrollbar {
+      /*滚动条整体样式*/
+      width: 4px;
+      height: 1px;
+      background-color: rgb(255, 234, 195);
+    }
+    &::-webkit-scrollbar-thumb {
+      /*滚动条里面小方块*/
+      border-radius: 10px;
+      background: rgb(255, 166, 0);
+    }
+    &::-webkit-scrollbar-track {
+      /*滚动条里面轨道*/
+      border-radius: 10px;
+    }
+    img {
+        width: 100%;
+    }
+  }
+}
+</style>
