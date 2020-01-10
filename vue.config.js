0e17f6ec7146757d477f4bb841312f9c1452b179
WANNA <2457684290@qq.com>
2020-01-10 14:25:29 +0800

    'config.js'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const path = require('path');
 module.exports = {
     outputDir: 'dist',
     // publicPath: '/test',
-    // publicPath: '/hisenseweb',
-    // publicPath: 'http://www.huizhangongsi.com/hisensePro2/',
-    publicPath: 'http://open.huizhangongsi.com/hisenseweb'
+    // publicPath: '/hisenseweb'
 }
