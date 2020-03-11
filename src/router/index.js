417f2b61f4b4282891ca2b081fa3b60da63a9610
zjj <2457684290@qq.com>
2020-03-11 21:42:38 +0800

    '路由文件更改'

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Total from '../views/Total.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    mode: 'history',
+    // mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
             path: '/cert/:id',
