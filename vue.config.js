789ebf0709c3a99513bf6728285dc701d041cdc7
WANNA <2457684290@qq.com>
2020-04-20 15:20:15 +0800

    'error页面'

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     // outputDir: 'dist',
-    publicPath: '/hisenseAir',
+    publicPath: './',
     configureWebpack: {
         plugins: [
             new CopyWebpackPlugin([
-                { from: path.join(__dirname, 'src/wx_api'), to: path.join(__dirname, 'dist/wx_api') },
-                { from: path.join(__dirname, 'src/.idea'), to: path.join(__dirname, 'dist/.idea') },
+                { from: path.join(__dirname, 'src/wx_api'), to: path.join(__dirname, 'dist/wx_api') }
             ])
         ]
     }
